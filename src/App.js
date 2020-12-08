d9cd6ba52da4ba085812793e1d58529090d255e0
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2020-12-08 16:52:30 +0530

    feat: added the songs page and styling for it

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { songList } from "./songsData";
+
+const displaySongs = (genre) => {
+  return (
+    <ul className="list-non-bullet-songs">
+      {songList[genre].map((song) => {
+        return (
+          <div key={song.title} className="flex-container">
+            <img className="image" src={song.image} alt="j"></img>
+            <div className="song-content">
+              <li className="list-item-inline-songs song-title">
+                <span>{song.title}</span>
+              </li>
+              <li className="list-item-inline-songs song-artist">
+                <span>{song.artists}</span>
+              </li>
+              <li className="list-item-inline-songs song-rating">
+                <span>{song.rating}</span>
+              </li>
+              <li className="list-item-inline-songs song-link">
+                <a href={song.link}>
+                  <i className="bx bxl-spotify bx-md"></i>
+                </a>
+              </li>
+            </div>
+          </div>
+        );
+      })}
+    </ul>
+  );
+};
+
+const genres = Object.keys(songList);
+
+export default function App() {
+  const [genre, setGenre] = useState("ALL");
+
+  const onClickHandler = (selectedGenre) => {
+    setGenre(selectedGenre);
+  };
+
+  const musicTabs = () => {
+    return (
+      <ul className="list-non-bullet-tabs">
+        {genres.map((g) => {
+          return (
+            <li className="list-item-inline-tabs" key={g}>
+              <button
+                key={g}
+                className="genre-button"
+                onClick={() => onClickHandler(g)}
+              >
+                {g}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="heading">
+        <h2> ELECTRONIC DANCE MUSIC (EDM) GENRES </h2>
+        <h4>
+          Top 3 Songs from each genre of EDM music are displayed below. Go ahead
+          and play your favourite songs from the spotify links.
+        </h4>
+      </div>
+      <div>{musicTabs()}</div>
+      <div>
+        {genres.map((g) => {
+          return <span key={g}>{genre === g && displaySongs(g)}</span>;
+        })}
+      </div>
+    </div>
+  );
+}
